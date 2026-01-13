e4eece4b391c395b438594f4acb8ffad0036edac
Devendra Pandey <pandey.devendra@indiamart.com>
2026-01-13 19:55:23 +0530

    Authentication using better auth

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,8 +1,14 @@
-import { PrismaClient } from '@prisma/client'
+import { Pool } from 'pg'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from "../generated/prisma/client";
 
+const connectionString = `${process.env.DATABASE_URL}`
 
+const pool = new Pool({ connectionString })
+const adapter = new PrismaPg(pool)
 const db = globalThis.prisma || new PrismaClient({
-  log: ['query', 'info', 'warn', 'error'], 
+  adapter,
+  log: ['query', 'info', 'warn', 'error']
 })
 
 if (process.env.NODE_ENV === 'development') {
