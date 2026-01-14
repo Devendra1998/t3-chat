0f47586c3c261bc8c55afb3a3d9c4add133c1fb3
Devendra Pandey <pandey.devendra@indiamart.com>
2026-01-14 11:34:16 +0530

    Home page chat layout

--- src/lib/db.js
+++ src/lib/db.js
@@ -4,6 +4,10 @@ import { PrismaClient } from "../generated/prisma/client";
 
 const connectionString = `${process.env.DATABASE_URL}`
 
+if (!connectionString) {
+  throw new Error("DATABASE_URL environment variable is required");
+}
+
 const pool = new Pool({ connectionString })
 const adapter = new PrismaPg(pool)
 const db = globalThis.prisma || new PrismaClient({
